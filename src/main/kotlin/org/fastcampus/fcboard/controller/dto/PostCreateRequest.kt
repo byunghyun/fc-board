74cc2a0f26401c09cde67a45f9177dbce8b52093
byunghyun <eer3481@naver.com>
2025-08-26 16:26:08 +0900

    FEAT: Add Post entities with basic fields.
    Signed-off-by: byunghyun <eer3481@naver.com>

---
+++ src/main/kotlin/org/fastcampus/fcboard/controller/dto/PostCreateRequest.kt
@@ -0,0 +1,7 @@
+package org.fastcampus.fcboard.controller.dto
+
+data class PostCreateRequest(
+    val title: String,
+    val content: String,
+    val createdBy: String,
+)
---
+++ src/main/kotlin/org/fastcampus/fcboard/controller/dto/PostDetailResponse.kt
@@ -0,0 +1,11 @@
+package org.fastcampus.fcboard.controller.dto
+
+import java.time.LocalDateTime
+
+data class PostDetailResponse(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val createdBy: String,
+    val createdAt: LocalDateTime,
+)
